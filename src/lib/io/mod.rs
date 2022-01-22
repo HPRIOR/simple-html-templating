93d774f59770b9c1f59544c93b286cb47aa9521b
HARRY PRIOR <harryprior@hotmail.co.uk>
2022-01-22 12:15:21 +0000

    Remove unnecessary lifetime, clean up imports

--- src/lib/io/mod.rs
+++ src/lib/io/mod.rs
@@ -1,4 +1,6 @@
-use std::{fs, io, io::Read, path::PathBuf};
+use std::fs;
+use std::io::Read;
+use std::path::PathBuf;
 use std::error::Error;
 use std::fs::DirEntry;
 
@@ -15,7 +17,7 @@ fn get_file_name_from_entry(entry: &DirEntry) -> String{
     ).split(".").take(1).collect()
 }
 
-pub fn get_text_content<'a>(dir: PathBuf) -> Result<Vec<BlogPost>, Box<dyn Error>> {
+pub fn get_text_content(dir: PathBuf) -> Result<Vec<BlogPost>, Box<dyn Error>> {
     let entries = fs::read_dir(dir)?;
     let mut result: Vec<BlogPost> = Vec::new();
     for entry in entries {
