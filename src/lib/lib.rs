13b13a192ebd6c8bbda9a93e119645e9a93b15fd
HARRY PRIOR <harryprior@hotmail.co.uk>
2022-01-22 12:14:22 +0000

    Add basic setup for lib error handling

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,6 +1,22 @@
+use std::error::Error;
+use std::io::{stderr, Write};
+use std::path::PathBuf;
+
 mod document_parser;
 pub mod io;
 
-pub fn lib(){
+pub fn lib(dir: PathBuf) -> Result<(), Vec<&'static str>> {
+    let mut err_list: Vec<&'static str> = vec![];
 
+    if let Ok(text_content) = io::get_text_content(dir) {
+
+    } else {
+        err_list.push("Could not find html content")
+    };
+
+    if err_list.len() > 0 {
+        Err(err_list)
+    } else {
+        Ok(())
+    }
 }
