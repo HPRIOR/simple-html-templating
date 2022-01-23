595c89a2f88749e2f1a999b888a7885fc1bea266
HARRY PRIOR <harryprior@hotmail.co.uk>
2022-01-23 09:45:08 +0000

    Add simple template logic and test

--- src/lib/template/mod.rs
+++ src/lib/template/mod.rs
@@ -1,6 +1,35 @@
-use crate::shared::enums::HtmlBody;
+use crate::shared::enums::{HtmlBody, HtmlTemplate};
 use crate::shared::structs::HtmlPage;
 
-pub fn attach_bodies_to_tempate(bodies: &Vec<HtmlBody>, template: &String) -> Vec<HtmlPage> {
-    panic!("not impl")
+pub fn attach_bodies_to_template(bodies: &Vec<HtmlBody>, template: &String) -> Vec<HtmlTemplate> {
+    bodies.into_iter().map(|html_body| {
+        let html = match html_body { HtmlBody::Of(html_page) => html_page };
+        let content = template.replace("!body!", &html.content);
+        HtmlTemplate::Of(HtmlPage { name: html.name.clone(), content })
+    }).collect()
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::shared::enums::{HtmlBody, HtmlTemplate};
+    use crate::shared::structs::HtmlPage;
+    use crate::template::attach_bodies_to_template;
+
+    #[test]
+    pub fn will_replace_string_in_single_page() {
+        let body = HtmlPage { name: String::from("test"), content: String::from("<p >This is a test paragraph</p>") };
+        let template = String::from("Html !body! template");
+        let result = attach_bodies_to_template(&vec![HtmlBody::Of(body)], &template);
+        let expected = vec![
+            HtmlTemplate::Of(
+                HtmlPage {
+                    name: String::from("test"),
+                    content: String::from("Html <p >This is a test paragraph</p> template"),
+                })];
+        assert_eq!(result, expected)
+    }
+}
+
+
+
