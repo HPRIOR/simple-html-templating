8f4fc88c9556432e4c8aea6868c86978952c6108
HARRY PRIOR <harryprior@hotmail.co.uk>
2022-01-23 11:51:45 +0000

    Use result chaining with and then

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,8 +1,11 @@
 extern crate core;
 
+use std::error::Error;
 use std::path::PathBuf;
 
 use crate::document_parser::parse_documents;
+use crate::shared::enums::HtmlBody;
+use crate::template::attach_bodies_to_template;
 
 mod document_parser;
 pub mod io;
@@ -10,17 +13,10 @@ mod template;
 pub mod shared;
 
 pub fn lib(dir: PathBuf) -> Result<(), Vec<&'static str>> {
-    let mut err_list: Vec<&'static str> = vec![];
+    let result =
+        io::get_text_content(dir)
+            .map(|blog_posts| parse_documents(&blog_posts))
+            .and_then(|html_body| attach_bodies_to_template(&html_body, &String::from("template")));
 
-    if let Ok(blog_posts) = io::get_text_content(dir) {
-        let html_bodies = parse_documents(&blog_posts);
-    } else {
-        err_list.push("Could not find html content")
-    };
-
-    if err_list.len() > 0 {
-        Err(err_list)
-    } else {
-        Ok(())
-    }
+    panic!("")
 }
--- src/lib/template/mod.rs
+++ src/lib/template/mod.rs
@@ -1,18 +1,30 @@
+use std::error::Error;
+use std::string::ParseError;
+
 use crate::shared::enums::{HtmlBody, HtmlTemplate};
+use crate::shared::errors::TemplateError;
 use crate::shared::structs::HtmlPage;
 
-pub fn attach_bodies_to_template(bodies: &Vec<HtmlBody>, template: &String) -> Vec<HtmlTemplate> {
-    bodies.into_iter().map(|html_body| {
-        let html = match html_body { HtmlBody::Of(html_page) => html_page };
-        let content = template.replace("!body!", &html.content);
-        HtmlTemplate::Of(HtmlPage { name: html.name.clone(), content })
-    }).collect()
+pub fn attach_bodies_to_template(bodies: &Vec<HtmlBody>, template: &String) -> Result<Vec<HtmlTemplate>, Box<dyn Error>> {
+    let replacement_str = "!body!";
+    if !template.contains(replacement_str) {
+        Err(Box::from(TemplateError))
+    } else {
+        Ok(bodies.into_iter().map(|html_body| {
+            let html = match html_body { HtmlBody::Of(html_page) => html_page };
+            let content = template.replace("!body!", &html.content);
+            HtmlTemplate::Of(HtmlPage { name: html.name.clone(), content })
+        }).collect())
+    }
 }
 
 
 #[cfg(test)]
 mod tests {
+    use std::error::Error;
+
     use crate::shared::enums::{HtmlBody, HtmlTemplate};
+    use crate::shared::errors::TemplateError;
     use crate::shared::structs::HtmlPage;
     use crate::template::attach_bodies_to_template;
 
@@ -21,13 +33,25 @@ mod tests {
         let body = HtmlPage { name: String::from("test"), content: String::from("<p >This is a test paragraph</p>") };
         let template = String::from("Html !body! template");
         let result = attach_bodies_to_template(&vec![HtmlBody::Of(body)], &template);
-        let expected = vec![
+        let expected: Result<Vec<HtmlTemplate>, Box<dyn Error>> = Ok(vec![
             HtmlTemplate::Of(
                 HtmlPage {
                     name: String::from("test"),
                     content: String::from("Html <p >This is a test paragraph</p> template"),
-                })];
-        assert_eq!(result, expected)
+                })]);
+        assert_eq!(result.unwrap(), expected.unwrap())
+    }
+
+    #[test]
+    pub fn will_return_err_if_with_bad_template() {
+        let body = HtmlPage { name: String::from("test"), content: String::from("<p >This is a test paragraph</p>") };
+        let template = String::from("Html template");
+        let result = attach_bodies_to_template(&vec![HtmlBody::Of(body)], &template);
+        let expected = match result {
+            Ok(_) => { "" }
+            Err(err) => { "error" }
+        };
+        assert_eq!(expected, "error")
     }
 }
 
