05f0a6cd5bc16005abf7b5751ab0ccaa218b296c
HARRY PRIOR <harryprior@hotmail.co.uk>
2022-01-24 20:10:27 +0000

    Add index page generation

---
+++ src/lib/index_html/mod.rs
@@ -0,0 +1,132 @@
+use std::error::Error;
+
+use crate::HtmlFinal;
+use crate::shared::errors::TemplateError;
+use crate::shared::structs::HtmlPage;
+
+pub fn get_index_page(template: &String, html_pages: &Vec<HtmlFinal>) -> Result<HtmlFinal, Box<dyn Error>> {
+    let href_list = get_list_of_href(html_pages).join("");
+    let template_replacement_str = "!body!";
+    if !template.contains(template_replacement_str) {
+        return Err(Box::from(TemplateError));
+    };
+    Ok(
+        HtmlFinal::Of(HtmlPage {
+            name: String::from("index"),
+            content: template.replace(
+                template_replacement_str,
+                format!("<ul >{}</ul>", href_list).as_str(),
+            ),
+        })
+    )
+}
+
+fn get_list_of_href(html_pages: &Vec<HtmlFinal>) -> Vec<String> {
+    html_pages.into_iter().map(|html| {
+        let html_pg = match html { HtmlFinal::Of(html_pg) => html_pg };
+        format!("<li ><a href=\"{}.html\" ></a></li>", html_pg.name.clone())
+    }).collect()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::HtmlFinal;
+    use crate::shared::structs::HtmlPage;
+
+    fn get_html_input() -> Vec<HtmlFinal> {
+        let html_page_one = HtmlFinal::Of(
+            HtmlPage {
+                name: String::from("html_page_one"),
+                content: String::from("content of html_page_one"),
+            });
+        let html_page_two = HtmlFinal::Of(
+            HtmlPage {
+                name: String::from("html_page_two"),
+                content: String::from("content of html_page_two"),
+            });
+
+        vec![html_page_one, html_page_two]
+    }
+
+    mod get_list_of_href {
+        use crate::HtmlFinal;
+        use crate::index_html::get_list_of_href;
+        use crate::index_html::tests::get_html_input;
+        use crate::shared::structs::HtmlPage;
+
+        #[test]
+        fn should_return_list_of_html_with_embedded_html_name() {
+            let input = get_html_input();
+            let result = get_list_of_href(&input);
+            let expected = vec![
+                String::from("<li ><a href=\"html_page_one.html\" ></a></li>"),
+                String::from("<li ><a href=\"html_page_two.html\" ></a></li>"),
+            ];
+
+            assert_eq!(result, expected)
+        }
+    }
+
+    mod get_index_page {
+        use std::error::Error;
+
+        use crate::HtmlFinal;
+        use crate::index_html::get_index_page;
+        use crate::index_html::tests::get_html_input;
+        use crate::shared::structs::HtmlPage;
+
+        #[test]
+        fn should_return_correct_html_page() {
+            let html_input = get_html_input();
+            let template = String::from("html !body! template");
+            let result = match get_index_page(&template, &html_input) {
+                Ok(result) => result,
+                Err(_) => panic!()
+            };
+
+            let expected = HtmlFinal::Of(
+                HtmlPage {
+                    name: String::from("index"),
+                    content: String::from(
+                        "html <ul >\
+                        <li ><a href=\"html_page_one.html\" ></a></li><li ><a href=\"html_page_two.html\" ></a></li>\
+                        </ul> template"
+                    ),
+                });
+            assert_eq!(result, expected)
+        }
+
+        #[test]
+        fn should_replace_content_placeholder_with_no_html() {
+            let html_input: Vec<HtmlFinal> = vec![];
+            let template = String::from("html !body! template");
+            let result = match get_index_page(&template, &html_input) {
+                Ok(result) => result,
+                Err(_) => panic!()
+            };
+
+            let expected = HtmlFinal::Of(
+                HtmlPage {
+                    name: String::from("index"),
+                    content: String::from(
+                        "html <ul >\
+                        </ul> template"
+                    ),
+                });
+            assert_eq!(result, expected)
+        }
+
+        #[test]
+        fn should_return_err_if_no_template() {
+            let html_input: Vec<HtmlFinal> = vec![];
+            let template = String::from("html template");
+            let result = match get_index_page(&template, &html_input) {
+                Ok(_) => "ok",
+                Err(_) => "error"
+            };
+
+            assert_eq!(result, "error")
+        }
+    }
+}
