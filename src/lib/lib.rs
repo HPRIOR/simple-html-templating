5269e1d81426503b8384f9de2d47ef97fd805a1f
HARRY PRIOR <harryprior@hotmail.co.uk>
2022-01-31 22:51:10 +0000

    formatting

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -13,13 +13,9 @@ mod html_generator;
 
 pub fn lib(blog_dir: PathBuf, template_dir: PathBuf, save_dir: PathBuf, ctx_path: PathBuf) -> Result<(), Box<dyn Error>> {
     let ctx = io::get_context(ctx_path)?;
-
     let template = io::get_template(template_dir)?;
     let html_content = io::get_text_content(blog_dir)?;
-
     let html = generate_html(&html_content, &template, &ctx)?;
-
-
     Ok(io::save_html(save_dir, &html)?)
 }
 
