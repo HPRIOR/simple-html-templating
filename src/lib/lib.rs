3531d67114c9ef55949b6f206a31a7e41f4b54a7
HARRY PRIOR <harryprior@hotmail.co.uk>
2022-02-05 22:09:52 +0000

    breaking change

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -15,7 +15,9 @@ pub fn lib(blog_dir: PathBuf, template_dir: PathBuf, save_dir: PathBuf, ctx_path
     let ctx = io::get_context(ctx_path)?;
     let template = io::get_template(template_dir)?;
     let html_content = io::get_text_content(blog_dir)?;
-    let html = generate_html(&html_content, &template, &ctx)?;
+    let html = gener
+    ate_html(&html_content, &template, &ctx)?;
+
     Ok(io::save_html(save_dir, &html)?)
 }
 
